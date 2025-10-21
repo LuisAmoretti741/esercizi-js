c716121b1b5313bda5de853a98188691d48bb154
LuisYsrael741 <amorettiysrael3@gmail.com>
2025-10-21 18:23:03 +0200

    esercizi finito js

---
+++ script.js
@@ -0,0 +1,37 @@
+//1) chiedete all'utente il nome e fategli gli auguri di compleanno
+// const nome = prompt("Come ti chiami?");
+// alert ("Buon compleanno, " + nome + "!!!");
+
+//2) chiedete all'utente la base e l'esponente e calcolate la potenza
+// const base = prompt("Inserisci la base:");
+// baseNumber = Number(base);
+
+// const esponente = prompt("Inserisci l'esponente");
+// esponenteNumber = Number(esponente);
+
+// const potenza = baseNumber ** esponenteNumber;
+// alert(base + " elevato alla " + esponente + " = " + potenza);
+
+//3) chiedete all'utente un numero e ditegli se è pari o dispari (operatore modulo)
+// const numero = prompt("Inserisci un numero:");
+// numeroNumber = Number(numero);
+
+// if (numeroNumber % 2 === 0) {
+//     alert("Il numero " + numeroNumber + " è pari.");
+// } else {
+//     alert("Il numero " + numero + " è dispari.");
+// }
+
+//4) fate inserire all'utente l'età e se è maggiore o uguale a 18 rispondete "puoi entrare"
+// const età = prompt("Quanti anni hai?");
+// etàNumber = Number(età);
+
+// if (etàNumber >= 18) {
+//     alert("Puoi entrare.");
+// } else {
+//     alert("Mi dispiace, non puoi entrare.")
+// }
+
+//5) fate inserire all'utente una stringa e convertitela in maiuscolo (cercare su internet)
+// const testo = prompt("Inserisci una frase o una parola:")
+// alert("In maiuscolo: " + testo.toLocaleUpperCase());
